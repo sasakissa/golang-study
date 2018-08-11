c53e0071d4cc276b2b44ef6f0d571cf744bc3fc5
MitaRyosuke <>
2018-08-11 23:04:21 +0900

    add ex04-02

--- ch04/ex01/main.go
+++ ch04/ex01/main.go
@@ -2,15 +2,29 @@ package main
 
 import (
 	"crypto/sha256"
+	"crypto/sha512"
+	"flag"
 )
 
 func main() {
-	c1 := sha256.Sum256([]byte("x"))
-	c2 := sha256.Sum256([]byte("X"))
-	println(DiffCount(c1, c2))
+	mode := flag.String("mode", "sha256", "mode flag")
+	if *mode == "sha256" {
+		c1 := sha256.Sum256([]byte("x"))
+		c2 := sha256.Sum256([]byte("X"))
+		println(DiffCount256(c1, c2))
+	} else if *mode == "sha384" {
+		c1 := sha512.Sum384([]byte("x"))
+		c2 := sha512.Sum384([]byte("X"))
+		println(DiffCount512(c1, c2))
+	} else if *mode == "sha512" {
+		c1 := sha512.Sum512([]byte("x"))
+		c2 := sha512.Sum512([]byte("X"))
+		println(DiffCount512(c1, c2))
+	}
+
 }
 
-func DiffCount(c1, c2 [32]byte) int {
+func DiffCount256(c1, c2 [32]byte) int {
 	count := 0
 	for i := 0; i < len(c1); i++ {
 		x := int(c1[i])
@@ -21,6 +35,16 @@ func DiffCount(c1, c2 [32]byte) int {
 	return count
 }
 
+func DiffCount512(c1, c2 [64]byte) int {
+	count := 0
+	for i := 0; i < len(c1); i++ {
+		x := int(c1[i])
+		y := int(c2[i])
+		z := x ^ y
+		count += PopCount(z)
+	}
+	return count
+}
 func PopCount(z int) int {
 	count := 0
 	for z != 0 {
