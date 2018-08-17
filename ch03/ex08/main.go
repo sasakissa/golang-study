4331e5678c46c4f58e41027aeac42c9ac95d3393
MitaRyosuke <sasakissa@gmail.com>
2018-08-17 09:36:50 +0900

    add ex03-08

---
+++ ch03/ex08/main.go
@@ -0,0 +1,164 @@
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/png"
+	"math"
+	"math/big"
+	"math/cmplx"
+	"os"
+)
+
+func main() {
+	const (
+		xmin, ymin, xmax, ymax = -2, -2, +2, +2
+		width, height          = 1024, 1024
+	)
+
+	img := image.NewRGBA(image.Rect(0, 0, width, height))
+	for py := 0; py < height; py++ {
+		y := float64(py)/height*(ymax-ymin) + ymin
+		for px := 0; px < width; px++ {
+			x := float64(px)/width*(xmax-xmin) + xmin
+			z := complex(x, y)
+			// ここで精度を決める
+			img.Set(px, py, mandelbrotRat(z))
+		}
+	}
+	png.Encode(os.Stdout, img) // NOTE: ignoring errors
+}
+
+func mandelbrot(z complex128) color.Color {
+	const iterations = 200
+	const contrast = 15
+	var v complex128
+	for n := uint8(0); n < iterations; n++ {
+		v = v*v + z
+		if cmplx.Abs(v) > 2 {
+			switch {
+			case n > 50: // dark red
+				return color.RGBA{100, 0, 0, 255}
+			default:
+				// logarithmic blue gradient to show small differences on the
+				// periphery of the fractal.
+				logScale := math.Log(float64(n)) / math.Log(float64(iterations))
+				return color.RGBA{0, 0, 255 - uint8(logScale*255), 255}
+			}
+		}
+	}
+	return color.Black
+}
+
+func mandelbrot64(z complex128) color.Color {
+	const iterations = 200
+	const contrast = 15
+	var v complex64
+	for n := uint8(0); n < iterations; n++ {
+		v = v*v + complex64(z)
+		if cmplx.Abs(complex128(v)) > 2 {
+			switch {
+			case n > 50: // dark red
+				return color.RGBA{100, 0, 0, 255}
+			default:
+				// logarithmic blue gradient to show small differences on the
+				// periphery of the fractal.
+				logScale := math.Log(float64(n)) / math.Log(float64(iterations))
+				return color.RGBA{0, 0, 255 - uint8(logScale*255), 255}
+			}
+		}
+	}
+	return color.Black
+}
+
+func mandelbrotBigFloat(z complex128) color.Color {
+	const iterations = 200
+	const contrast = 15
+	zR := (&big.Float{}).SetFloat64(real(z))
+	zI := (&big.Float{}).SetFloat64(imag(z))
+	var vR, vI = &big.Float{}, &big.Float{}
+	for i := uint8(0); i < iterations; i++ {
+		// (r+i)^2 = r^2 + 2ri + i^2
+		vR2, vI2 := &big.Float{}, &big.Float{}
+		vR2.Mul(vR, vR).Sub(vR2, (&big.Float{}).Mul(vI, vI)).Add(vR2, zR)
+		vI2.Mul(vR, vI).Mul(vI2, big.NewFloat(2)).Add(vI2, zI)
+		vR, vI = vR2, vI2
+		squareSum := &big.Float{}
+		squareSum.Mul(vR, vR).Add(squareSum, (&big.Float{}).Mul(vI, vI))
+		if squareSum.Cmp(big.NewFloat(4)) == 1 {
+			switch {
+			case i > 50: // dark red
+				return color.RGBA{100, 0, 0, 255}
+			default:
+				// logarithmic blue gradient to show small differences on the
+				// periphery of the fractal.
+				logScale := math.Log(float64(i)) / math.Log(float64(iterations))
+				return color.RGBA{0, 0, 255 - uint8(logScale*255), 255}
+			}
+		}
+	}
+	return color.Black
+}
+
+func mandelbrotRat(z complex128) color.Color {
+	// High-resolution images take an extremely long time to render with
+	// iterations = 200. Multiplying arbitrary precision numbers has
+	// algorithmic complexity of at least O(n*log(n)*log(log(n)))
+	// (https://en.wikipedia.org/wiki/Arbitrary-precision_arithmetic#Implementation_issues).
+	const iterations = 200
+	const contrast = 15
+	zR := (&big.Rat{}).SetFloat64(real(z))
+	zI := (&big.Rat{}).SetFloat64(imag(z))
+	var vR, vI = &big.Rat{}, &big.Rat{}
+	for i := uint8(0); i < iterations; i++ {
+		// (r+i)^2 = r^2 + 2ri + i^2
+		vR2, vI2 := &big.Rat{}, &big.Rat{}
+		vR2.Mul(vR, vR).Sub(vR2, (&big.Rat{}).Mul(vI, vI)).Add(vR2, zR)
+		vI2.Mul(vR, vI).Mul(vI2, big.NewRat(2, 1)).Add(vI2, zI)
+		vR, vI = vR2, vI2
+		squareSum := &big.Rat{}
+		squareSum.Mul(vR, vR).Add(squareSum, (&big.Rat{}).Mul(vI, vI))
+		if squareSum.Cmp(big.NewRat(4, 1)) == 1 {
+			switch {
+			case i > 50:
+				return color.RGBA{100, 0, 0, 255}
+			default:
+
+				logScale := math.Log(float64(i)) / math.Log(float64(iterations))
+				return color.RGBA{0, 0, 255 - uint8(logScale*255), 255}
+			}
+		}
+	}
+	return color.Black
+}
+
+func acos(z complex128) color.Color {
+	v := cmplx.Acos(z)
+	blue := uint8(real(v)*128) + 127
+	red := uint8(imag(v)*128) + 127
+	return color.YCbCr{192, blue, red}
+}
+
+func sqrt(z complex128) color.Color {
+	v := cmplx.Sqrt(z)
+	blue := uint8(real(v)*128) + 127
+	red := uint8(imag(v)*128) + 127
+	return color.YCbCr{128, blue, red}
+}
+
+// f(x) = x^4 - 1
+//
+// z' = z - f(z)/f'(z)
+//    = z - (z^4 - 1) / (4 * z^3)
+//    = z - (z - 1/z^3) / 4
+func newton(z complex128) color.Color {
+	const iterations = 37
+	const contrast = 7
+	for i := uint8(0); i < iterations; i++ {
+		z -= (z - 1/(z*z*z)) / 4
+		if cmplx.Abs(z*z*z*z-1) < 1e-6 {
+			return color.Gray{255 - contrast*i}
+		}
+	}
+	return color.Black
+}
