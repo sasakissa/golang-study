95545c1216e65ee946eef9f26997adf40dfd47db
MitaRyosuke <sasakissa@gmail.com>
2018-08-17 09:46:10 +0900

    add ex04-08

--- ch04/ex08/main.go
+++ ch04/ex08/main.go
@@ -9,12 +9,11 @@ import (
 	"unicode/utf8"
 )
 
-// 文字や数字などを数えるように修正する
 func main() {
-	letterCounts := make(map[rune]int) // counts of Unicode characters
-	digitCounts := make(map[rune]int)  // counts of Unicode number
-	var utflen [utf8.UTFMax + 1]int    // count of lengths of UTF-8 encodings
-	invalid := 0                       // count of invalid UTF-8 characters
+	counts := make(map[rune]int) // counts of Unicode characters
+	cats := make(map[string]int)
+	var utflen [utf8.UTFMax + 1]int // count of lengths of UTF-8 encodings
+	invalid := 0                    // count of invalid UTF-8 characters
 
 	in := bufio.NewReader(os.Stdin)
 	for {
@@ -30,21 +29,16 @@ func main() {
 			invalid++
 			continue
 		}
-
-		if unicode.IsDigit(r) {
-			digitCounts[r]++
+		for catName, rangeTable := range unicode.Properties {
+			if unicode.In(r, rangeTable) {
+				cats[catName]++
+			}
 		}
-		if unicode.IsLetter(r) {
-			letterCounts[r]++
-		}
+		counts[r]++
 		utflen[n]++
 	}
-	fmt.Printf("letter\tcount\n")
-	for c, n := range letterCounts {
-		fmt.Printf("%q\t%d\n", c, n)
-	}
-	fmt.Printf("digit\tcount\n")
-	for c, n := range digitCounts {
+	fmt.Printf("rune\tcount\n")
+	for c, n := range counts {
 		fmt.Printf("%q\t%d\n", c, n)
 	}
 	fmt.Print("\nlen\tcount\n")
@@ -52,6 +46,10 @@ func main() {
 		if i > 0 {
 			fmt.Printf("%d\t%d\n", i, n)
 		}
+	}
+	fmt.Printf("\n%-30s count\n", "category")
+	for cat, n := range cats {
+		fmt.Printf("%-30s %d\n", cat, n)
 	}
 	if invalid > 0 {
 		fmt.Printf("\n%d invalid UTF-8 characters\n", invalid)
