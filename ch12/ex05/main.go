6737c7de9d04bfd05aea1f422ee3f4fa142cf4a4
ryosuke_mita <sasakissa@gmail.com>
2018-12-21 10:16:06 +0900

    12-05

---
+++ ch12/ex05/main.go
@@ -0,0 +1,139 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	type Movie struct {
+		Title, Subtitle string
+		Year            int
+		Actor           map[string]string
+		Oscars          []string
+		Sequel          *string
+	}
+	strangelove := Movie{
+		Title:    "Dr. Strangelove",
+		Subtitle: "How I Learned to Stop Worrying and Love the Bomb",
+		Year:     1964,
+		Actor: map[string]string{
+			"Dr. Strangelove":            "Peter Sellers",
+			"Grp. Capt. Lionel Mandrake": "Peter Sellers",
+			"Pres. Merkin Muffley":       "Peter Sellers",
+			"Gen. Buck Turgidson":        "George C. Scott",
+			"Brig. Gen. Jack D. Ripper":  "Sterling Hayden",
+			`Maj. T.J. "King" Kong`:      "Slim Pickens",
+		},
+		Oscars: []string{
+			"Best Actor (Nomin.)",
+			"Best Adapted Screenplay (Nomin.)",
+			"Best Director (Nomin.)",
+			"Best Picture (Nomin.)",
+		},
+	}
+
+	// Encode it
+	data, _ := Marshal(strangelove)
+	fmt.Println(string(data))
+}
+
+//!+Marshal
+// Marshal encodes a Go value in S-expression form.
+func Marshal(v interface{}) ([]byte, error) {
+	var buf bytes.Buffer
+	if err := encodeJson(&buf, reflect.ValueOf(v)); err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
+
+//!-Marshal
+
+// encode writes to buf an S-expression representation of v.
+//!+encode
+func encodeJson(buf *bytes.Buffer, v reflect.Value) error {
+	switch v.Kind() {
+	case reflect.Invalid:
+		buf.WriteString("\"nil\"")
+
+	case reflect.Int, reflect.Int8, reflect.Int16,
+		reflect.Int32, reflect.Int64:
+		fmt.Fprintf(buf, "%d", v.Int())
+
+	case reflect.Uint, reflect.Uint8, reflect.Uint16,
+		reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+		fmt.Fprintf(buf, "%d", v.Uint())
+
+	case reflect.String:
+		fmt.Fprintf(buf, "%q", v.String())
+
+	case reflect.Ptr:
+		return encodeJson(buf, v.Elem())
+
+	case reflect.Array, reflect.Slice: // (value ...)
+		buf.WriteByte('[')
+		for i := 0; i < v.Len(); i++ {
+			if i > 0 {
+				buf.WriteString(", ")
+			}
+			if err := encodeJson(buf, v.Index(i)); err != nil {
+				return err
+			}
+		}
+		buf.WriteByte(']')
+
+	case reflect.Struct: // ((name value) ...)
+		buf.WriteByte('{')
+		for i := 0; i < v.NumField(); i++ {
+			if i > 0 {
+				buf.WriteString(", ")
+			}
+			fmt.Fprintf(buf, "\"%s\" : ", v.Type().Field(i).Name)
+			if err := encodeJson(buf, v.Field(i)); err != nil {
+				return err
+			}
+			// buf.WriteByte('')
+		}
+		buf.WriteByte('}')
+
+	case reflect.Map: // ((key value) ...)
+		buf.WriteByte('{')
+		for i, key := range v.MapKeys() {
+			if i > 0 {
+				buf.WriteString(", ")
+			}
+			if err := encodeJson(buf, key); err != nil {
+				return err
+			}
+			buf.WriteString(": ")
+			if err := encodeJson(buf, v.MapIndex(key)); err != nil {
+				return err
+			}
+		}
+		buf.WriteByte('}')
+
+	case reflect.Bool:
+		if v.Bool() {
+			fmt.Fprintf(buf, "\"t\"")
+		} else {
+			fmt.Fprintf(buf, "\"nil\"")
+		}
+	case reflect.Float32, reflect.Float64:
+		fmt.Fprintf(buf, "%g", v.Float())
+
+	case reflect.Complex64, reflect.Complex128:
+		c := v.Complex()
+		fmt.Fprintf(buf, "#C(%g, %g)", real(c), imag(c))
+
+	case reflect.Interface:
+		fmt.Fprintf(buf, "(%q", reflect.Indirect(v).Type())
+		encodeJson(buf, reflect.Indirect(v).Elem())
+		buf.WriteByte(')')
+
+	default: // float, complex, bool, chan, func, interface
+		return fmt.Errorf("unsupported type: %s", v.Type())
+	}
+	return nil
+}
